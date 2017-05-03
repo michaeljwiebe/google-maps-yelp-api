62874aabcacdce8094b4faeb1a21067b172b3185
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-03 17:50:11 -0400

    cleaned code, added setInterval

--- scripts.js
+++ scripts.js
@@ -1,20 +1,7 @@
-// Use the endpoint at https://pelp-api-wdi.herokuapp.com/markers to get all markers stored in a database. The database currently holds zero markers. Markers store information related to a business, using yelp data and its own data. 
-// Create a user interface that searches the yelp API at https://pelp-api-wdi.herokuapp.com/search, using term and loca
-// Create an interface that allows a user to click a result and save it to the database, using information from the yelp result and additional information that the user adds (type of place).
-// To create a marker in the database, send a request to the same endpoint used to get the markers from the database, but using a POST request. The data that needs to be passed to a Marker are:
-// Name refers to the business’ name, marker_type refers to the type of establishment it is (cafe, lunch, pizza, bagels, etc.)
-// Using the Google Maps API, show a map on the page that has a map marker for every Marker in the database. If a new Marker is added to the database, a new map marker should appear on the map.
-// If a new Marker is added to the database from another user’s browser, the map should automatically update on any other user’s browser. 
-// Store the Markers returned from the database in Marker objects
-// Each marker type should have an icon associated with it in place of the default google maps marker. Markers with type coffee should have a coffee icon, pizza should have a pizza icon. 
-
-
 var search = document.getElementsByClassName("search")[0];
 var results = document.getElementsByClassName("results")[0];
-var markersObject;
 var populateMarkers = document.getElementsByClassName("populateMarkers")[0];
 var map;
-
 
 
 search.addEventListener("click", function(){
@@ -26,7 +13,7 @@ populateMarkers.addEventListener("click", function(){
 	getFromDatabase();
 })
 
-
+setInterval(function(){getFromDatabase()}, 1000);
 
 function searchYelp(searchText, searchLocation){
     $.ajax({
@@ -51,7 +38,7 @@ function listResults(responseObj){
 	var i = 0;
 	responseObj.businesses.forEach(function(business){
 		var j = i;
-		results.innerHTML += "<div class='business business" + i + "'><p class='bizName'>" + business.name + "</p><p class='bizAddress'>" + business.location.address[0] + "</p></div>";
+		results.innerHTML += "<div class='business business" + i + "'><div class='biz-text'><p class='bizName'>" + business.name + "</p><p class='biz-address'>" + business.location.address[0] + "</p></div><div class='biz-img'";
 		setTimeout(function(){
 			var businessDiv = document.getElementsByClassName("business" + j)[0];
 			addButtons(businessDiv, business)
@@ -93,8 +80,8 @@ function getFromDatabase(){
         data: {
         },
         success: function(response){
-            markersObject = response;
             addMarkers(response);
+            console.log(response);
         }
     })
 }
@@ -113,7 +100,7 @@ function addMarkers(markersObj){
 }
 
 function initMap(){
-	var philly = {lat: 39.9691826, lng: -75.1338785};
+	var philly = {lat: 39.96204, lng: -75.17222};
 
 	map = new google.maps.Map(document.getElementsByClassName('map')[0], {
 		zoom: 12,
